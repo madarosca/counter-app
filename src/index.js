1cc7f83757f6a2d9ab5b96e81189ff587bd83da9
Madalina Rosca <madalina@neho.ch>
2023-01-26 22:22:02 +0200

    add routes

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import { CountersProvider } from './contexts/counters.context';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
 	<React.StrictMode>
-		<CountersProvider>
-			<App />
-		</CountersProvider>
+		<BrowserRouter>
+			<CountersProvider>
+				<App />
+			</CountersProvider>
+		</BrowserRouter>
 	</React.StrictMode>
 );
 
