0ea5dd734f30a5aeecb859ce13fd6fbef3911592
Madalina Rosca <madalina@neho.ch>
2023-01-26 15:08:46 +0200

    add firebase for deploy

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyA6x0-7VnmE9Kk2Wu8LFUtOaG_5qKlnzpw',
+	authDomain: 'counter-app-3b2d5.firebaseapp.com',
+	projectId: 'counter-app-3b2d5',
+	storageBucket: 'counter-app-3b2d5.appspot.com',
+	messagingSenderId: '20507672111',
+	appId: '1:20507672111:web:de1dc8da20f8937e9c90da',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
