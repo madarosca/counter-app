c415fe3cc5d0380f705e1b86068d56061bd96b8c
Madalina Rosca <madalina@neho.ch>
2023-01-30 22:13:52 +0200

    remove sw

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import { CountersProvider } from './contexts/counters.context';
 import { BrowserRouter } from 'react-router-dom';
@@ -18,5 +17,3 @@ root.render(
 		</BrowserRouter>
 	</React.StrictMode>
 );
-
-registerServiceWorker();
